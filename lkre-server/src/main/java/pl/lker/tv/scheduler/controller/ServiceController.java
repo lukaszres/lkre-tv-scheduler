4076a1d0c920d0e31a99ba2eed17ab79ecc2345b
lkre <lukasz.res@gmail.com>
2023-01-22 11:05:29 +0100

    #5 Wrong sorting

--- lkre-server/src/main/java/pl/lker/tv/scheduler/controller/ServiceController.java
+++ lkre-server/src/main/java/pl/lker/tv/scheduler/controller/ServiceController.java
@@ -2,12 +2,12 @@ package pl.lker.tv.scheduler.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.lker.tv.scheduler.generator.HtmlService;
 import pl.lker.tv.scheduler.generator.service.HtmlServiceImpl;
 import pl.lker.tv.scheduler.model.Seance;
 import pl.lker.tv.scheduler.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.LinkedList;
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 public class ServiceController {
+    public static final long HOUR = 3600 * 1000; // in milli-seconds.
 
     @GetMapping("/service")
     public User serv() {
@@ -32,11 +33,11 @@ public class ServiceController {
 
     private List<Seance> getSeances() {
         List<Seance> seances = new LinkedList<>();
-        seances.add(new Seance("title1", new Date(), "genre1", "episode", "channel1"));
-        seances.add(new Seance("title2", new Date(), "genre2", "episode", "channel1"));
-        seances.add(new Seance("title3", new Date(), "genre", "episode", "channel1"));
-        seances.add(new Seance("title4", new Date(), "genre1", "episode", "channel1"));
-        seances.add(new Seance("title5", new Date(), "genre", "episode", "channel1"));
+        seances.add(new Seance("title2", new Date(new Date().getTime() + 2 * HOUR), "genre2", "episode", "channel1"));
+        seances.add(new Seance("title1", new Date(new Date().getTime() + 1 * HOUR), "genre1", "episode", "channel1"));
+        seances.add(new Seance("title4", new Date(new Date().getTime() + 4 * HOUR), "genre1", "episode", "channel1"));
+        seances.add(new Seance("title3", new Date(new Date().getTime() + 3 * HOUR), "genre", "episode", "channel1"));
+        seances.add(new Seance("title5", new Date(new Date().getTime() + 1 * HOUR), "genre", "episode", "channel1"));
         return seances;
     }
 
