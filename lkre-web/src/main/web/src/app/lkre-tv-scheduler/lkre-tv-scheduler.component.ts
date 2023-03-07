2baf3f7fdb39ea5795ff073fb0a4c578c3459f29
lkre <lukasz.res@gmail.com>
2023-03-07 08:03:57 +0100

    LTS-15 Get seances list after click button

--- lkre-web/src/main/web/src/app/lkre-tv-scheduler/lkre-tv-scheduler.component.ts
+++ lkre-web/src/main/web/src/app/lkre-tv-scheduler/lkre-tv-scheduler.component.ts
@@ -27,6 +27,9 @@ export class LkreTvSchedulerComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  downloadSeances() {
     this.getSeances().subscribe(seances => {
       this.allSeances = seances.sort((a, b) => (a.time > b.time) ? 0 : -1);
       this.selectedSeances = seances;
@@ -34,21 +37,21 @@ export class LkreTvSchedulerComponent implements OnInit {
     })
   }
 
-  getSeances(): Observable<Seance[]> {
+  private getSeances(): Observable<Seance[]> {
     return this.httpClient.get<Seance[]>('/seances');
   }
 
   onToppingsChanged() {
     let genres: string[] = this.toppings.value;
     this.selectedSeances = this.allSeances.filter(value => genres.find(e => e === value.genre));
-    this.selectedSeancesString = this.seancesToString(this.selectedSeances);
+    this.selectedSeancesString = this.seancesToString();
   }
 
-  seancesToString(selectedSeances: Seance[]): string {
+  private seancesToString(): string {
     return this.selectedSeances.map(value => this.seanceToString(value)).join('\n');
   }
 
-  seanceToString(seance: Seance): string {
+  private seanceToString(seance: Seance): string {
     let episode = seance.episode != null ? this.SEPARATOR + seance.episode : "";
     return formatDate(seance.time, "hh:mm", "en-US") + this.SEPARATOR + seance.channel + this.SEPARATOR + seance.genre + this.SEPARATOR + seance.title + episode;
   }
